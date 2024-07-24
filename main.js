ff001a321024c6fd2f6f9a0af9b33741227c9039
Mangal81 <mangalkumar548744@gmail.com>
2024-07-24 12:34:24 +0530

    The todo app is ready to use now

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
 let form = document.getElementById("form");
-let input = document.getElementById("input");
+let title = document.getElementById("title");
+let description = document.getElementById("description");
 let msg = document.getElementById("errorMsg");
 let todos = document.getElementById("todos");
 
-
-
 form.addEventListener("submit",(event)=>{
 	event.preventDefault();
-	//console.log(input.value);
-	validateInput();
+	//console.log(title.value);
+	validatetitle();
 })
 
-let validateInput = ()=>{
-	if(input.value===''){
-		msg.innerHTML="Input can not be empty";
+let validatetitle = ()=>{
+	if(title.value==='' || description.value===""){
+		msg.innerHTML="Title or Description can not be empty.";
 	}
 	else{
 		console.log("good");
 		acceptData();
-		creatTodo();
+		msg.innerHTML="";
 	}
 }
 
-let data = {};
+let data = [];
 
 let acceptData = ()=>{
-	data['title']=input.value;
+	// data['title']=title.value;
+	// data['description']=description.value;
+	data.push({
+		title:title.value,
+		description:description.value
+	})
+	localStorage.setItem("data",JSON.stringify(data));
+	creatTodo();
+
 	console.log(data);
 }
 
 let deleteData = (e)=>{
 	e.parentElement.parentElement.remove();
+	data.splice(e.parentElement.parentElement.id,1);
+	localStorage.setItem("data",JSON.stringify(data));
+	console.log(data, localStorage)
 }
 
 let updateData = (e)=>{
-	input.value = e.parentElement.previousElementSibling.innerHTML;
+	let selectedTodo = e.parentElement.parentElement;
+	title.value = selectedTodo.children[0].innerHTML;
+	description.value = selectedTodo.children[1].innerHTML;
+	// console.log(selectedTodo);
 	deleteData(e);
 }
 
 let creatTodo = ()=>{
-	todos.innerHTML += 
-	`<div>
-		<p>${data.title}</p>
-		<span class="options">
-			<button onclick="updateData(this)">Edit</button>
-			<button onclick="deleteData(this)">Delete</button>
-		</span>
-	</div>`;
-	input.value="";
+	todos.innerHTML ="";
+	data.map((value, index)=>{
+		return (todos.innerHTML += 
+			`<div id="${index}">
+				<h4>${value.title}</h4>
+				<p>${value.description}</p>
+				<span class="options">
+					<button onclick="updateData(this)">Edit</button>
+					<button onclick="deleteData(this);creatTodo()">Delete</button>
+				</span>
+			</div>`);
+	})
+	
+	resetInputs();
 }
+
+let resetInputs = ()=>{
+	title.value="";
+	description.value="";
+}
+
+// IIFE
+
+(()=>{
+	data=JSON.parse(localStorage.getItem("data")) || [];
+	creatTodo();
+	console.log(data);
+})()
